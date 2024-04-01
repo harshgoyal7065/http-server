e75b1235109923ff257199bc0038737f7b95ac3c
Harsh Goyal <harsh.goyal@joshtechnologygroup.com>
2024-04-02 00:57:52 +0530

    Returning data

--- app/main.js
+++ app/main.js
@@ -6,20 +6,19 @@ console.log("Logs from your program will appear here!");
 
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
-    const req = data.toString().split("\r\n");
+    const req = data.toString().split(CRLF);
     const path = req[0].split(" ")[1].trim();
     if (path.startsWith("/echo/")) {
       const val = path.substring(6);
       return socket.write(
-        "HTTP/1.1 200 OK\r\n" +
-          "Content-Type: text/plain\r\n" +
-          "Content-Length: " +
-          val.length +
-          "\r\n\r\n" +
-          val +
-          "\r\n"
+        `HTTP/1.1 200 OK${CRLF}Content-Type: text/plain${CRLF}Content-Length:${val.length}${CRLF}${CRLF}${val}${CRLF}`
       );
     }
+    if (path === "" || path === "/") {
+        socket.write(`HTTP/1.1 200 OK${CRLF}${CRLF}`);
+      } else {
+        socket.write(`HTTP/1.1 404 Not Found${CRLF}${CRLF}`);
+      }
   });
 
   socket.on("close", () => {
