a812d7fd7ce601e08e64d488b29a4801ca956040
Harsh Goyal <harsh.goyal@joshtechnologygroup.com>
2024-04-02 01:13:04 +0530

    Post a file

--- app/main.js
+++ app/main.js
@@ -1,85 +1,151 @@
-
 const net = require("net");
-const fs = require('fs');
+const fs = require("fs");
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-const [nodePath, scriptPath, ...args] = process.argv;
-function getArgValue(argName) {
-    return args[args.findIndex(arg => arg === '--directory') + 1];
-}
+const HTTP_VERSION = "HTTP/1.1";
+const CR = "\r";
+const LF = "\n";
+const CRLF = CR + LF;
 
-// Uncomment this to pass the first stage
+//HTTP Status code and status string map
+const HTTP_STATUS_CODE = {
+  200: "OK",
+  404: "Not Found",
+};
+
+//Parse the request data
+const parseRequest = (data) => {
+  //Split the data by CRLF
+  const parsedBody = data.toString().split(CRLF);
+
+  //Parse the request line
+  const [method, path, httpVersion] = parsedBody[0].split(" ");
+
+  //Get all the headers in JSON format
+  const bodyStartIndex = parsedBody.findIndex((val) => val === "");
+  const headersString = parsedBody.slice(1, bodyStartIndex);
+  const headers = getJsonFromStringArray(headersString);
+
+  const body =
+    parsedBody.length > bodyStartIndex + 1
+      ? parsedBody[bodyStartIndex + 1]
+      : undefined;
+
+  return {
+    method,
+    path,
+    httpVersion,
+    headers,
+    body,
+  };
+};
+
+//Generate response status line based on status code
+const getResponseStatusLine = (statusCode) => {
+  return (
+    HTTP_VERSION +
+    " " +
+    statusCode +
+    " " +
+    HTTP_STATUS_CODE[statusCode] +
+    " " +
+    CRLF
+  );
+};
+
+//Convert string to JSON
+const getStringFromJson = (json) => {
+  return (
+    Object.entries(json)
+      .map(([key, value]) => `${key}: ${value}`)
+      .join(CRLF) + CRLF
+  );
+};
+
+//Convert array of strings to JSON
+const getJsonFromStringArray = (arr) => {
+  return arr.reduce((acc, curr) => {
+    const [key, value] = curr.split(":").map((val) => val.trim());
+    return {
+      ...acc,
+      [key]: value,
+    };
+  }, {});
+};
+
+//Generate Representation Headers
+const generateRepresentationHeaders = (body, headers) => {
+  const headerJson = {
+    "Content-Type": "text/plain",
+    "Content-Length": body?.length ?? 0,
+    ...headers,
+  };
+
+  return getStringFromJson(headerJson);
+};
+
+//Generate response string
+const generateResponse = (statusCode, body, headers = {}) => {
+  let res = getResponseStatusLine(statusCode);
+  res += generateRepresentationHeaders(body, headers);
+  res += !!body ? LF + body : CRLF;
+  return res;
+};
+
 const server = net.createServer((socket) => {
-    socket.on("data", (data) => {
-        const [__headers, _body] = data.toString().split("\r\n\r\n");
-        const _headers = __headers.split('\r\n');
-        const firstLine = _headers.shift().split(' ');
-        const [method, path, pro] = firstLine
-        const headers = {};
-        _headers.forEach((header) => {
-            const [key, value] = header.split(':')
-            headers[key.trim()] = value.trim()
-        })
-        if (path.startsWith('/files/')) {
-            const fileName = path.replace('/files/', '');
-            const filePath = `${fs.realpathSync(getArgValue('--directory'))}/${fileName}`
-            if(!fs.existsSync(filePath)){
-                socket.write("HTTP/1.1 404 OK\r\n\r\n");
-                socket.end();
-                return;
-            }
-            const fileContent = fs.readFileSync(filePath).toString()
-            const headers = [
-                'HTTP/1.1 200 OK',
-                'Content-Type: application/octet-stream',
-                `Content-Length: ${fileContent.length}`,
-            ]
-            let response = headers.join("\r\n");
-            response += '\r\n\r\n';
-            response += fileContent;
-            socket.write(response);
-            socket.end();
-1
-            return;
-        }
-        if (path.startsWith('/echo/')) {
-            const text = path.replace('/echo/', '');
-            const headers = [
-                'HTTP/1.1 200 OK',
-                'Content-Type: text/plain',
-                `Content-Length: ${text.length}`,
-            ]
-            let response = headers.join("\r\n");
-            response += '\r\n\r\n';
-            response += text;
-            socket.write(response);
-            socket.end();
-            return;
-        }
-        if (path === '/user-agent') {
-            const resHeaders = [
-                'HTTP/1.1 200 OK',
-                'Content-Type: text/plain',
-                `Content-Length: ${headers['User-Agent'].length}`,
-            ]
-            let response = resHeaders.join("\r\n");
-            response += '\r\n\r\n';
-            response += headers['User-Agent'];
-            socket.write(response);
-            socket.end();
-            return;
-        }
-        if (path === '/') {
-            socket.write("HTTP/1.1 200 OK\r\n\r\n");
-            socket.end();
-            return;
-        }
-        socket.write("HTTP/1.1 404 OK\r\n\r\n");
-        socket.end();
-    });
-    socket.on("close", () => {
-        socket.end();
-        server.close();
-    });
+  socket.on("close", () => {
+    socket.end();
+    server.close();
+  });
+
+  //Read data from the client
+  socket.on("data", (buffer) => {
+    const {
+      method,
+      path,
+      headers: reqHeaders,
+      body: reqBody,
+    } = parseRequest(buffer);
+
+    let body;
+    let headers = {};
+    let statusCode = 404;
+
+    if (path === '/') {
+      statusCode = 200;
+    } else if (path.includes("/echo/")) {
+      body = path.split("/echo/")[1];
+    } else if (path === "/user-agent") {
+      body = reqHeaders["User-Agent"];
+    } else if (path.includes("/files/")) {
+      const filePath = process.argv[3] + path.split("/files/")[1];
+      
+      if (method === "GET") {
+        try {
+          const file = fs.readFileSync(filePath);
+          headers = {
+            "Content-Type": "application/octet-stream",
+          };
+          body = file;
+        } catch (e) {}
+      } else if (method === "POST") {
+        fs.writeFileSync(filePath, reqBody);
+        statusCode = 201;
+      }
+    }
+
+    if (body) {
+      statusCode = 200;
+    }
+
+    const res = generateResponse(
+      statusCode,
+      body,
+      headers
+    );
+
+    //Respond to the client
+    socket.write(res);
+  });
 });
+
 server.listen(4221, "localhost");
