ef6071dac27501d5c42589382e2f518d3fde51a4
Harsh Goyal <harsh.goyal@joshtechnologygroup.com>
2024-04-02 00:48:19 +0530

    Data connection

--- app/main.js
+++ app/main.js
@@ -1,9 +1,14 @@
 const net = require("net");
+const { CRLF } = require("./constants");
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
 const server = net.createServer((socket) => {
+  socket.on("data", (data) => {
+    socket.write(`HTTP/1.1 200 OK${CRLF}${CRLF}`);
+  });
+
   socket.on("close", () => {
     socket.end();
     server.close();
